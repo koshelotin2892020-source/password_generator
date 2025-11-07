6ffceafeac95e302fc17fd86cbb8931ba2756945
Alex <koshelotin2892020@gmail.com>
2025-11-07 15:19:12 +0300

    Инит для пакета passgen

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -1,1 +1,16 @@
 # __init__.py
+
+from .generator import PasswordGenerator
+from .storage import PasswordStorage
+from .utils import validate_length, hash_password
+from .commands import handle_generate, handle_find, handle_list
+
+__all__ = [
+    'PasswordGenerator',
+    'PasswordStorage',
+    'validate_length',
+    'hash_password',
+    'handle_generate',
+    'handle_find',
+    'handle_list'
+    ]
